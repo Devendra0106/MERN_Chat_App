f8e32af2905e135c38821494105a1fd6a7200510
Devendra <devendramorankar@gmail.com>
2023-07-05 00:42:37 +0530

    added user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const chats = require('./data/data');
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
-const app = express();
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('API is running Successfully!');
+connectDB();
+const app = express();
+
+app.use(express.json()); // to accept JSON data
+
+app.get("/", (req, res) => {
+	res.send("API is running Successfully!");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-})
+app.use("/api/user", userRoutes);
 
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server started on PORT ${PORT}`))
+app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));

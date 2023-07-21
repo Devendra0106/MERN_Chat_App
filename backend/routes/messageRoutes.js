d11a1d739c5fd782b563374ba75cd9ebe9a55a37
Devendra <devendramorankar@gmail.com>
2023-07-22 01:00:49 +0530

    created send and get Message API's

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const {
+	sendMessage,
+	allMessage,
+} = require("../controllers/messageControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// router.route("/").post(protect, sendMessage);
-// router.route("/:chatId").get(protect, allMessage);
+router.route("/").post(protect, sendMessage);
+router.route("/:chatId").get(protect, allMessage);
 
 module.exports = router;
